48dd2b5496c4179eac8f2b9eb948ffa865e00cca
firefox-star <firefox-star@users.noreply.github.com>
2026-07-31 15:05:44 +0000

    fix: add missing getStore() to sharedState - dashboard was crashing silently

--- sharedState.js
+++ sharedState.js
@@ -1,25 +1,29 @@
 const EventEmitter = require('events');
 
 class BotState extends EventEmitter {
-	constructor() {
-		super();
-		this.sock = null;
-		this.store = null;
-		this.startTime = Date.now();
-	}
+        constructor() {
+                super();
+                this.sock = null;
+                this.store = null;
+                this.startTime = Date.now();
+        }
 
-	setSock(sock) {
-		this.sock = sock;
-		this.emit('connected');
-	}
+        setSock(sock) {
+                this.sock = sock;
+                this.emit('connected');
+        }
 
-	getSock() {
-		return this.sock;
-	}
+        getSock() {
+                return this.sock;
+        }
 
-	setStore(store) {
-		this.store = store;
-	}
+        setStore(store) {
+                this.store = store;
+        }
+
+        getStore() {
+                return this.store;
+        }
 }
 
 module.exports = new BotState();
